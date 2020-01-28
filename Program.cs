4d3dc0774ccbb6a4c02eb6ef5691aa27983166ee
Kiara Reeves <60400531+gitAClue22@users.noreply.github.com>
2020-01-28 12:17:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace schooltuition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            double sum = 6000; 
+            do
+            {
+                double x = sum;
+                double y = x * .02;
+                 sum = x + y;
+                x = sum;
+
+
+                Console.WriteLine($"Your tuition for year {n} is ${sum}.");
+                n++;
+            }
+
+            while (n <= 5); 
+
+
+
+            
+
+            
+
+            
+
+
+        }
+    }
+}
